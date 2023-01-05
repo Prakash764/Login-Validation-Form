e5016f936462679859b6cf9a3996236f2dea9b60
Prakash Tekam <121537372+Prakash764@users.noreply.github.com>
2023-01-05 08:52:21 -0800

    Add files via upload

---
+++ HTML,CSS & Javascript/forgot.js
@@ -0,0 +1,13 @@
+<div>
+    <div class="form login">
+    <div class="form-content">
+        <input type="email" placeholder="Email" className="input">Email</input>
+    </div>
+    <div>
+        <input type="password" placeholder="New Password" className="password">New Password</input>
+    </div>
+    <div class="field button-field">
+        <button class="button">Reset Password</button>
+    </div>
+</div>
+</div>
